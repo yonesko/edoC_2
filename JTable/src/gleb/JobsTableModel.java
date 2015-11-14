9fd4f4ec84a8e842c808efe76f1978a64ff76052
Glebushka <yonesko@gmail.com>
2015-11-14 20:56:47 +0300

    tested JTable - TableModel

---
+++ JTable/src/gleb/JobsTableModel.java
@@ -0,0 +1,44 @@
+package gleb;
+
+import javax.swing.event.TableModelListener;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JobsTableModel extends AbstractTableModel {
+    private List<List<String>> data;
+
+    public JobsTableModel() {
+        System.out.println("JobsTableModel");
+        data = new ArrayList<>();
+    }
+
+
+    @Override
+    public int getRowCount() {
+//        System.out.println("getRowCount");
+        return data.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+//        System.out.println("getColumnCount");
+        return 2;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        System.out.println("getValueAt");
+        return data.get(rowIndex).get(columnIndex);
+    }
+
+    public void insertRow(String a, String b) {
+        System.out.println("insertRow");
+        List<String> l = new ArrayList<>();
+        l.add(a);
+        l.add(b);
+        data.add(l);
+        fireTableRowsInserted(data.size() - 1, data.size() - 1);
+    }
+}
