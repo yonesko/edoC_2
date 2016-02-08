6152b0bb52aa4bd873f5f67648eb0408aacbb396
Danichev Gleb <gd@mils.solutions>
2016-02-08 12:59:13 +0300

    par substitute done

---
+++ ParSubs/src/main/java/ParSubs.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ParSubs {
+    public static void main(String...atgs) throws Exception {
+        Scanner in = new Scanner(System.in);
+        String query = "";
+        Map<String, String> parToVal = new HashMap<String, String>();
+
+        //read query
+        String s;
+        while (in.hasNext() && !(s=in.nextLine()).contains("Query parameters")) {
+            query += s + '\n';
+        }
+        if (query.length() == 0)
+            throw new Exception("Empty query");
+        //read params
+        String e[];
+        while (in.hasNext()) {
+            e = in.nextLine().split("\\s+");
+            parToVal.put(e[0], e[1]);
+        }
+        if (parToVal.size() == 0)
+            throw new Exception("Empty parameters");
+        //substitute params in query with values
+        String val;
+        for (Map.Entry<String, String> entry : parToVal.entrySet()) {
+            val = entry.getValue();
+            if (val.equals("null") == false)
+                val = '\'' + val + '\'';
+            query = query.replaceAll(entry.getKey(), val);
+        }
+        //show rsult
+        System.out.println("--- Result ---");
+        System.out.println(query);
+    }
+}
