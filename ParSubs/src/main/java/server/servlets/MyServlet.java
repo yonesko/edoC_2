fd468d7d1fc3760b366d199b6fc61ac52c021b1d
Danichev Gleb <gd@mils.solutions>
2016-02-18 14:59:44 +0300

    par substitute done

--- ParSubs/src/main/java/server/servlets/MyServlet.java
+++ ParSubs/src/main/java/server/servlets/MyServlet.java
@@ -11,9 +11,11 @@ import java.io.IOException;
 
 
 public class MyServlet extends HttpServlet {
+    static int i = 0;
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html;charset=utf-8");
+        System.out.println(i++);
         try {
             resp.getWriter().println(ParSubs.subs(req.getParameter("SQLtext")));
         } catch (Exception e) {
