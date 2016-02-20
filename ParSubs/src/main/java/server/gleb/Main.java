f56c41ac799ac50988ebee8023d11733cdca44f8
Danichev Gleb <gd@mils.solutions>
2016-02-20 13:00:54 +0300

    prod

--- ParSubs/src/main/java/server/gleb/Main.java
+++ ParSubs/src/main/java/server/gleb/Main.java
@@ -25,7 +25,7 @@ public class Main {
         HandlerList handlers = new HandlerList();
         handlers.setHandlers(new Handler[]{resource_handler, context});
 
-        Server server = new Server(8081);
+        Server server = new Server(8080);
         server.setHandler(handlers);
 
         server.start();
