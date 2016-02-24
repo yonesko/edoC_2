a5f83dd27f9b32f77b61f559b4456e1fccd8f95c
Danichev Gleb <gd@mils.solutions>
2016-02-24 15:54:16 +0300

    added formater

--- ParSubsTest/src/main/java/server/gleb/Main.java
+++ ParSubsTest/src/main/java/server/gleb/Main.java
@@ -9,28 +9,29 @@ import org.eclipse.jetty.servlet.ServletHolder;
 import server.servlets.TransServl;
 import server.servlets.IziJarDownloaderServl;
 
+import java.io.PrintWriter;
+
 public class Main {
     private static final int PORT_POSITION = 0;
+    private static int port;
     public static void main(String...argv) throws Exception {
-        System.out.println("Working Directory = " +
-                System.getProperty("user.dir"));
         TransServl transServl = new TransServl();
         IziJarDownloaderServl scriptServlet = new IziJarDownloaderServl();
-
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         ResourceHandler resource_handler = new ResourceHandler();
-
+        //map servlets
         context.addServlet(new ServletHolder(transServl), "/trans");
         context.addServlet(new ServletHolder(scriptServlet), "/IziJar.bash");
 
         resource_handler.setResourceBase("public_html");
-
+        //add servlets to Jetty
         HandlerList handlers = new HandlerList();
         handlers.setHandlers(new Handler[]{resource_handler, context});
 
-        Server server = new Server(Integer.parseInt(argv[Main.PORT_POSITION]));
+        port = Integer.parseInt(argv[PORT_POSITION]);
+        Server server = new Server(port);
         server.setHandler(handlers);
-
+        //start server
         server.start();
         System.out.println("Server started");
         server.join();
--- ParSubsTest/src/main/java/server/gleb/ProcessSQL.java
+++ ParSubsTest/src/main/java/server/gleb/ProcessSQL.java
@@ -1,11 +1,21 @@
 package server.gleb;
 
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class ProcessSQL {
-    public static String substituteParams(String SQL) {
+    public static String substituteParams(String SQL) throws IOException {
         Scanner in = new Scanner(SQL);
         String query = "";
         Map<String, String> parToVal = new HashMap<String, String>();
@@ -39,6 +49,48 @@ public class ProcessSQL {
             }
         }
         //show result
-        return query;
+        return formatSQL(query);
+    }
+    private static String formatSQL(String query) throws IOException {
+        String url = "http://sqlformat.org/api/v1/format";
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+//        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        String urlParameters = "keyword_case=upper&reindent=1&sql="+query;
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        if (responseCode != 200) {
+            System.err.println("sqlformat.org query not OK");
+            return query;
+        }
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        JSONObject jResult = new JSONObject(response.toString());
+        return jResult.getString("result");
     }
 }
