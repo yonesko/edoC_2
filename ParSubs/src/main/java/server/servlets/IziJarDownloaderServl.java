491a84d28b431286117838580b928b34e27d4642
Danichev Gleb <gd@mils.solutions>
2016-03-03 17:46:24 +0300

    added utilit page

--- ParSubs/src/main/java/server/servlets/IziJarDownloaderServl.java
+++ ParSubs/src/main/java/server/servlets/IziJarDownloaderServl.java
@@ -1,21 +1,36 @@
 package server.servlets;
 
+import org.rauschig.jarchivelib.ArchiveFormat;
+import org.rauschig.jarchivelib.Archiver;
+import org.rauschig.jarchivelib.ArchiverFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class IziJarDownloaderServl extends HttpServlet {
-    static String path = "/home/gleb/bin/IziJar.bash";
+    static String source[] = {
+            "/home/gleb/Documents/codingGame/iZiPatch/target/IziPatch.jar",
+            "/home/gleb/bin/iZiPatch.bash"
+    };
+    static String archiveName = "iZiPatch";
+    static String archivePath = "/tmp";
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("send " + path);
-        resp.setContentType("application/x-tar");
+        System.out.println("sending utilita");
+        //send file
+        resp.setContentType("application/gzip");
         OutputStream out = resp.getOutputStream();
-        FileInputStream in = new FileInputStream(path);
+        FileInputStream in = new FileInputStream(makeArchive());
         byte[] buffer = new byte[4096];
         int length;
         while ((length = in.read(buffer)) > 0){
@@ -23,5 +38,15 @@ public class IziJarDownloaderServl extends HttpServlet {
         }
         in.close();
         out.flush();
+    }
+
+    private File makeArchive() throws IOException {
+        List<File> src = new ArrayList<File>();
+        for (String s : source)
+            src.add(new File(s));
+
+        Archiver archiver = ArchiverFactory.createArchiver(ArchiveFormat.ZIP);
+        File archive = archiver.create(archiveName, new File(archivePath), src.toArray(new File[src.size()]));
+        return archive;
     }
 }
