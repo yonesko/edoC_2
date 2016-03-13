507c5029ab382df1685377d6ca3c46ece8e44fb3
Glebushka <yonesko@gmail.com>
2016-03-13 15:13:44 +0300

    l 5 1 tests mock mbean; done

--- stepic/web-service-Java/stepic_java_webserver-master/gleb/L5.1 Tests/src/main/java/example/Main.java
+++ stepic/web-service-Java/stepic_java_webserver-master/gleb/L5.1 Tests/src/main/java/example/Main.java
@@ -12,6 +12,7 @@ import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AdminPageServlet;
 import servlets.HomePageServlet;
 
 import javax.management.MBeanServer;
@@ -21,25 +22,22 @@ import java.lang.management.ManagementFactory;
 public class Main {
     static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String...args) throws Exception {
-        if (args.length != 1) {
-            logger.error("Ukazi port");
-            System.exit(1);
-        }
 
-        int port = Integer.valueOf(args[0]);
+        int port = 8081;
 
         logger.info("Starting at http://127.0.0.1:" + port);
 
-        AccountServerI accountServer = new AccountServer(1);
+        AccountServerI accountServer = new AccountServer(10);
 
         AccountServerControllerMBean serverStat = new AccountServerController(accountServer);
         MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-        ObjectName objectName = new ObjectName("L5.1:type=AccountServerController");
+        ObjectName objectName = new ObjectName("Admin:type=AccountServerController");
         mbs.registerMBean(serverStat, objectName);
 
         Server server = new Server(port);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(new HomePageServlet(accountServer)), HomePageServlet.PAGE_URL);
+        context.addServlet(new ServletHolder(new AdminPageServlet(accountServer)), AdminPageServlet.PAGE_URL);
 
         ResourceHandler resourceHandler = new ResourceHandler();
         resourceHandler.setDirectoriesListed(true);
@@ -52,6 +50,7 @@ public class Main {
 
         server.start();
         logger.info("Server started");
+        System.out.println("Server started");
         server.join();
     }
 }
