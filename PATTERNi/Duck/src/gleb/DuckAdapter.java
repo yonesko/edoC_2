78edd33edde92f09099b0a607f22582d5a16557b
Danichev Gleb <G.Danichev@mils-s.ru>
2016-05-27 15:21:20 +0300

    "Adapter" pattern

---
+++ PATTERNi/Duck/src/gleb/DuckAdapter.java
@@ -0,0 +1,19 @@
+package gleb;
+
+public class DuckAdapter implements Turkey {
+    private Duck duck;
+
+    public DuckAdapter(Duck duck) {
+        this.duck = duck;
+    }
+
+    @Override
+    public void gobble() {
+        duck.quack();
+    }
+
+    @Override
+    public void fly() {
+        duck.fly();
+    }
+}
---
+++ PATTERNi/Duck/src/gleb/Turkey.java
@@ -0,0 +1,6 @@
+package gleb;
+
+public interface Turkey {
+    void gobble();
+    void fly();
+}
